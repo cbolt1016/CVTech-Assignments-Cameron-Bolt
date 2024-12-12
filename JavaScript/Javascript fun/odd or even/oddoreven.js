49424e1c159df30b9508e515257ce7c6a4df78d9
Cam <camtheman16oct@gmail.com>
2024-12-12 13:45:29 -0600

    Add files via upload

---
+++ JavaScript/Javascript fun/odd or even/oddoreven.js
@@ -0,0 +1,8 @@
+function check() {
+    var v = document.getElementById('evenoddinput').value;
+    if (v % 2 == 0) {
+      console.log("Even");
+    } else {
+      console.log("odd");
+    }
+  }
